b0f164148f84f565cfede20d7cd73a7f05abb46f
Bruno Inec <brunoinec@gmail.com>
2018-05-23 19:51:49 +0200

    make password plugin work

--- lockatme/unlockers/password.py
+++ lockatme/unlockers/password.py
@@ -1,14 +1,13 @@
-import time
 import getpass
-
 import pamela as pam
 
 
 def authenticate():
     user = getpass.getuser()
-    password = getpass.getpass(prompt='')
-    if pam.authenticate(user, password):
-        return True
-
-    time.sleep(15)
-    return True
+    while True:
+        password = getpass.getpass(prompt='')
+        try:
+            pam.authenticate(user, password)
+            return True
+        except pam.PAMError:
+            continue
